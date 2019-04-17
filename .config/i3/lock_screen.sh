7dc9968627c83fb9b0bcd8a59fbc37e3b655f9b1
Joel <rootjdev@gmail.com>
2019-04-17 08:52:23 +0200

    adding notification to the lockscreen script

--- .config/i3/lock_screen.sh
+++ .config/i3/lock_screen.sh
@@ -5,6 +5,7 @@
 #itrap revert HUP INT TERM
 #xset +dpms dpms 0 0 5
 #i3lock -e -c 2f343f
+notify-send "locking the screen now" --icon="user-away"
 import -silent -window root png:- | mogrify -blur 0x10 png:- | composite -gravity South -geometry -20x1200 ~/.config/i3/ricknmorty.png png:- png:- >/tmp/lock.png
 i3lock -n -e -u -i /tmp/lock.png &
 echo $!>~/.i3lock
