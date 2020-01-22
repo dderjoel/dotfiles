6a71a4b748bddba9369198e97c4f4eed35cb3a55
Joel <rootjdev@gmail.com>
2020-01-22 06:01:33 +0100

    pause

--- .config/i3/lock_screen.sh
+++ .config/i3/lock_screen.sh
@@ -5,13 +5,9 @@
 #itrap revert HUP INT TERM
 #xset +dpms dpms 0 0 5
 #i3lock -e -c 2f343f
-ls /tmp/lockpipe* >/dev/null
-if [ $? -gt 0 ]; then
-    mkfifo /tmp/lockpipe
-    mkfifo /tmp/lockpipe2
-fi 
 #import -silent -window root png:/tmp/lockpipe &
-pausing audio
+
+#pausing audio
 playerctl pause
 #mogrify -blur 0x10 png:- </tmp/lockpipe | composite -gravity South -geometry -20x1200 ~/.config/i3/ricknmorty.png png: /tmp/lockpipe2 &
 #i3lock -n -e -c 123456 #-i /tmp/lockpipe2 &
