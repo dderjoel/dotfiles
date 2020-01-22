e13d49485825d5151cca7b99a99902fb2a2c79d9
Joel <rootjdev@gmail.com>
2020-01-22 06:02:10 +0100

    updating in background, then only dim lockscreen

--- .config/i3/lock_screen.sh
+++ .config/i3/lock_screen.sh
@@ -11,9 +11,8 @@
 playerctl pause
 #mogrify -blur 0x10 png:- </tmp/lockpipe | composite -gravity South -geometry -20x1200 ~/.config/i3/ricknmorty.png png: /tmp/lockpipe2 &
 #i3lock -n -e -c 123456 #-i /tmp/lockpipe2 &
-betterlockscreen -l dimblur &
-betterlockscreen -u $(ls ~/Pictures/lock/* | sort -R | head -n1) &
-
+betterlockscreen -u $(ls ~/Pictures/lock/* | sort -R | head -n1) -r $(xrandr | grep Screen | awk '{print $8 "x" $10}') &
+betterlockscreen -l dim 
 
 #echo $!>~/.i3lock
 
