7854323982a2ac2a9ffac41ce192edc21f211098
Joel <rootjdev@gmail.com>
2021-02-23 10:06:57 +1030

    updated xrandr cmds for dock

--- screenlayout/default.screenlayout.sh
+++ screenlayout/default.screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x840 --rotate normal --output DP-1 --mode 1920x1200 --pos 3840x0 --rotate left --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 1920x1080 --pos 1920x840 --rotate normal
+xrandr -d :0.0 --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-2 --primary --mode 2560x1440 --pos 1920x480 --rotate normal --output DP-1-1 --mode 1920x1080 --pos 4480x0 --rotate left --output DP-1-3 --mode 1920x1200 --pos 0x720 --rotate normal
