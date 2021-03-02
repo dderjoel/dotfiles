c8fd2dab6ebd4eef898da8b4ee186c40a82b49a3
Joel <rootjdev@gmail.com>
2021-03-03 09:28:20 +1030

    making screenlayout script more robust
    Signed-off-by: Joel <rootjdev@gmail.com>

--- screenlayout/default.screenlayout.sh
+++ screenlayout/default.screenlayout.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
+echo "default $(date)" >> /home/joel/dotfiles/screenlayout/invoked.log
 xrandr -d :0.0 --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-2 --primary --mode 2560x1440 --pos 1920x480 --rotate normal --output DP-1-1 --mode 1920x1080 --pos 4480x0 --rotate left --output DP-1-3 --mode 1920x1200 --pos 0x720 --rotate normal
--- screenlayout/single.screenlayout.sh
+++ screenlayout/single.screenlayout.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-xrandr -d :0.0 --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+#!/usr/bin/env sh
+echo "single $(date)" >> /home/joel/dotfiles/screenlayout/invoked.log
+xrandr -d :0.0 --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
+
+for d in $(xrandr | grep disconnected | cut -d" " -f1); do
+    xrandr -d :0.0 --output $d --off
+done
