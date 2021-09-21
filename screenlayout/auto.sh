b3d65dfd9e72e3740062e6e293257c8a30603e6c
Joel <rootjdev@gmail.com>
2021-09-21 10:11:27 +0930

    yet another try to get the screens working... it often works ish...

--- screenlayout/auto.sh
+++ screenlayout/auto.sh
@@ -1,45 +1,34 @@
 #!/usr/bin/env bash
 
+
 # this scrpit will check if there are enough monitors for setting the ./default.screenlayout.sh. if not, we will execute the ./single.screenlayout.sh
 # add a udev rule like this:
 # echo 'SUBSYSTEM=="power_supply", RUN+="/home/joel/dotfiles/screenlayout/auto.sh"' > /etc/udev/rules.d/90-dock.rules
 
-# cheers on this guy for the convenience functions https://stackoverflow.com/a/1985512/4361749
-LOCKFILE="/var/lock/`basename $0`"
-LOCKFD=99
+[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
 
-# PRIVATE
-_lock()             { flock -$1 $LOCKFD; }
-_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
-_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
-
-# ON START
-_prepare_locking
-
-# PUBLIC
-exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
-exlock()            { _lock x; }   # obtain an exclusive lock
-shlock()            { _lock s; }   # obtain a shared lock
-unlock()            { _lock u; }   # drop a lock
-# End cheers
-
-
-# Simplest example is avoiding running multiple instances of script.
-exlock_now || exit 1
-
-echo "sleeping"
-sleep 3;
-echo "working"
+# export XAUTHORITY=/home/joel/.XAUTHORITY
 
 date >> /home/joel/dotfiles/screenlayout/invoked.log 
+echo "sleeping"
+sleep 5;
+echo "working"
 
-xrandr | grep -q DP-1-1
-[[ $? -eq 1 ]] && /home/joel/dotfiles/screenlayout/single.screenlayout.sh && exit 0
-xrandr | grep -q DP-1-2
-[[ $? -eq 1 ]] && /home/joel/dotfiles/screenlayout/single.screenlayout.sh && exit 0 
-xrandr | grep -q DP-1-3
-[[ $? -eq 1 ]] && /home/joel/dotfiles/screenlayout/single.screenlayout.sh && exit 0 
+num_connected=0
+xrr=`xrandr`
 
-/home/joel/dotfiles/screenlayout/default.screenlayout.sh && exit 0 
-
+grep -q "DP-1-1 connected" <<< ${xrr}
+[[ $? -eq 0 ]] && num_connected=$(( $num_connected + 1 ))
+grep -q "DP-1-2 connected" <<< ${xrr}
+[[ $? -eq 0 ]] && num_connected=$(( $num_connected + 1 ))
+grep -q "DP-1-3 connected" <<< ${xrr}
+[[ $? -eq 0 ]] && num_connected=$(( $num_connected + 1 ))
+echo numconnected_in_auto$num_connected >> /home/joel/dotfiles/screenlayout/invoked.log 
+if [[ ${num_connected} -eq 3 ]]; then
+    /home/joel/dotfiles/screenlayout/default.screenlayout.sh
+    exit 0
+else
+    /home/joel/dotfiles/screenlayout/single.screenlayout.sh
+    exit 0
+fi
 exit 1
