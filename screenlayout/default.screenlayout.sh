d04c5c5a268d586bd81c1cb7c7d8fed08c543ba7
Joel <rootjdev@gmail.com>
2022-04-26 16:19:16 +0930

    unified / fix layout

--- screenlayout/default.screenlayout.sh
+++ screenlayout/default.screenlayout.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-echo "default $(date)" >> /home/joel/dotfiles/screenlayout/invoked.log
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-1 --mode 1920x1200 --pos 4240x0 --rotate left --output DP-1-2 --primary --mode 2560x1440 --pos 1680x480 --rotate normal --output DP-1-3 --mode 1680x1050 --pos 0x870 --rotate normal
+set -e
 
-sleep 1 && i3-msg restart
+xrandr \
+  --output eDP-1 --off \
+  --output DP-1 --off \
+  --output DP-2 --off \
+  --output HDMI-1 --off \
+  --output HDMI-2 --off \
+  --output DP-1-1 --mode 2560x1440 --pos 2098x85 --rotate left --rate 59.95 \
+  --output DP-1-2 --mode 3840x2160 --pos 3538x485 --rotate normal --rate 29.98 --primary \
+  --output DP-1-3 --mode 1920x1200 --pos 7378x725 --rotate left --rate 59.95
+
+sleep .3 && i3-msg restart >/dev/null
--- screenlayout/single.screenlayout.sh
+++ screenlayout/single.screenlayout.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -e
 
+xrandr \
+  --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --primary \
+  --output DP-1 --off \
+  --output DP-2 --off \
+  --output HDMI-1 --off \
+  --output HDMI-2 --off \
+  --output DP-1-1 --off \
+  --output DP-1-2 --off \
+  --output DP-1-3 --off
 
-# for d in $(xrandr | grep disconnected | cut -d" " -f1); do
-# xrandr -d :0.0 --output $d --off
-# echo output offed fr $d
-# done
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
-
-# xrandr -d :0.0 --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
-echo "single done $(date)" >> /home/joel/dotfiles/screenlayout/invoked.log
-
-sleep 1 && i3-msg restart
+sleep .1 && i3-msg restart
