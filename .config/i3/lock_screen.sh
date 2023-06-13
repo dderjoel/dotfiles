15fd14686a87e3f235af718050b5129e8c2778c5
Joel <rootjdev@gmail.com>
2023-06-13 11:19:21 +0930

    syntax mostly

--- .config/i3/lock_screen.sh
+++ .config/i3/lock_screen.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-#exiting script, if i3lock is already running
-pgrep i3lock
-[[ ${?} -eq 0 ]] && exit 0;
+# exiting script, if i3lock is already running
+pgrep i3lock && exit 0
 
-#pausing audio 
+# pausing audio
 playerctl pause
 
 # https://unsplash.com/oauth/applications
-T=`mktemp`
+T=$(mktemp)
 curl \
-    -s \
-    -H "Authorization: ${ENV_UNSPLASH_KEY}" \
-    'https://api.unsplash.com/photos/random?query=dark+sky&orientation=landscape' \
-    | jq  ".urls.raw" \
-    | xargs curl --create-dirs -o ${T} && \
-    multilockscreen -u ${T} --dim 40 &
+  -s \
+  -H "Authorization: ${ENV_UNSPLASH_KEY}" \
+  'https://api.unsplash.com/photos/random?query=dark+sky&orientation=landscape' |
+  jq ".urls.raw" |
+  xargs curl --create-dirs -o "${T}" &&
+  multilockscreen -u "${T}" --dim 40 &
 
+# this way, we can call this script and only update the picture
+[[ "${1}" != update ]] && multilockscreen -l
 
-multilockscreen -l 
-
-#wait for the upate to be done.
+# wait for the upate to be done.
 wait
-#and remove the downloaded pic, since its now in ~/.cache, betterlockscreen took care of that.
-rm ${T}
+# and remove the downloaded pic, since it's now in ~/.cache, multilockscreen took care of that.
+rm "${T}"
