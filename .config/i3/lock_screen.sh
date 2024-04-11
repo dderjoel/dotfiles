352b252c8f4a34c15dc0e776d4eeb82bcd7279af
Joel <rootjdev@gmail.com>
2024-04-12 10:37:16 +1200

    pausing dunst when i3 lock is running

--- .config/i3/lock_screen.sh
+++ .config/i3/lock_screen.sh
@@ -6,6 +6,10 @@ pgrep i3lock && exit 0
 # pausing audio
 playerctl pause
 
+# pause_dunst
+notify-send "Pause notifications" &&
+  killall -SIGUSR1 dunst 2>&1
+
 # https://unsplash.com/oauth/applications
 T=$(mktemp)
 curl \
@@ -23,3 +27,8 @@ curl \
 wait
 # and remove the downloaded pic, since it's now in ~/.cache, multilockscreen took care of that.
 rm "${T}"
+
+# wait for multilockscreen to close and the nunpause dunst
+tail --pid="$(pidof multilockscreen)" -f /dev/null && killall -SIGUSR2 dunst
+# yes... why use tail?, I was surprised, too
+# https://stackoverflow.com/questions/1058047/wait-for-a-process-to-finish#41613532
